ad34ffee85c8bc930a20891b44c7148c9a9c8fe7
yhchan0918 <yhchan0918@gmail.com>
2021-03-04 14:42:09 +0800

    complete post(accommodation) component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import Post from './src/components/Post';
 import HomeScreen from './src/screens/Home';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <Post />
     </SafeAreaView>
   );
 }
