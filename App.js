fbb6caa3eb5f3eb751bead222ba09d5e31265a0e
yhchan0918 <yhchan0918@gmail.com>
2021-03-04 14:51:40 +0800

    refactor post compoent with injecting props

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Post from './src/components/Post';
 import HomeScreen from './src/screens/Home';
+import feed from './assets/data/feed';
 
 export default function App() {
   return (
     <SafeAreaView>
       {/* <HomeScreen /> */}
-      <Post />
+      <Post post={feed[0]} />
     </SafeAreaView>
   );
 }
